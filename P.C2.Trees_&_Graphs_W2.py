2d2ea833740e4fcc1c24c4815665520e138e71a5
jate-beatles <jate@live.cn>
2025-02-26 22:56:52 +0100

    go

--- P.C2.Trees_&_Graphs_W2.py
+++ P.C2.Trees_&_Graphs_W2.py
@@ -1,1 +1,13 @@
-#### 
+#### Graph Introduction 
+#       Relation are initi and nodes 
+#       1. no self-loops 
+#       2. Muliedge - 
+#
+####   Where do the graph come from?  - social network/computer network/ecologial relationwork/electricl circuite
+#            traversal problems /find the spinning trees/flow ....etc.
+#       1. Adjacency  matrix   
+#       2. Adjacency list representation  -!!!!!!!!!!!!!!!!!!!!! - Adjacency List Representation 
+#       3. Incidcence representation 
+
+
+#
