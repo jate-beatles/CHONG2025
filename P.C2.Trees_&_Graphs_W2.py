eb1fcaa0f52f960cdbb63bbb0e95044c259884ba
jate-beatles <jate@live.cn>
2025-03-08 23:48:00 +0100

    unonivel

--- P.C2.Trees_&_Graphs_W2.py
+++ P.C2.Trees_&_Graphs_W2.py
@@ -1,6 +1,6 @@
 #### Graph Introduction 
 #       Relation are initi and nodes 
-#       1. no self-loops 
+#       1. no self-loops   
 #       2. Muliedge - 
 #
 ####   Where do the graph come from?  - social network/computer network/ecologial relationwork/electricl circuite
@@ -10,4 +10,19 @@
 #       3. Incidcence representation 
 
 
+#  Vertic/node  -------- the connection between the node  ''' edge''''
+#  graph []e.g. the sereve is the node, the link is the edge
+#               google map  ------ street, ----- edge  
+
+
+
+#### How to represent the graph on the computer
+#       1. adjacency matrix  --- graph is define by the matrix, vertices is from the rows and column [n_ij] is representh the edge
+#           k nodes will have the k(k-1) edge, e.g., many of the matrix store the (0,0) 
+#       2. adjacency list representive --------every node points  to list of the neighbors  ---- adjacency node  ( thus there is not storage the 0.0)
+#           |k| + |m|  is the size of the adjacency list representative
+#       3. incidence matrix representive
 #
+
+####    Traversals --
+#       operation of the graph  - 
